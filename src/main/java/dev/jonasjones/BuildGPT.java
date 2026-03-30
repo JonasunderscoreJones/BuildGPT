3795db509ba5728c15db7142bdfd6ebf1f2b0052
Jonas_Jones <me@jonasjones.dev>
2026-03-30 19:27:40 +0200

    Cleaned up imports

--- src/main/java/dev/jonasjones/BuildGPT.java
+++ src/main/java/dev/jonasjones/BuildGPT.java
@@ -1,11 +1,8 @@
 package dev.jonasjones;
 
 import com.google.gson.*;
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -18,10 +15,8 @@ import net.minecraft.core.BlockPos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -33,8 +28,6 @@ import java.nio.file.StandardOpenOption;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.Vector;
 
 public class BuildGPT implements ModInitializer {
 	public static final String MOD_ID = "buildgpt";
